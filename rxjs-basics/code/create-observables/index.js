134e36c0994b0ac9f1a8d6110d5c9814caab26f3
Alain D'Ettorre <alain.det@gmail.com>
2020-05-26 01:31:17 +0200

    RxJs: Add fromEvent example

---
+++ rxjs-basics/code/create-observables/index.js
@@ -0,0 +1,17 @@
+import { fromEvent } from 'rxjs';
+
+const observer = {
+  next: (val) => console.log('next', val),
+  error: (err) => console.log('error', err),
+  complete: () => console.log('complete'),
+}
+
+// fromEvent
+const source$ = fromEvent(document, 'click');
+const subOne = source$.subscribe(observer);
+const subTwo = source$.subscribe(observer);
+
+setTimeout(() => {
+  console.log('unsubscribing');
+  subOne.unsubscribe();
+}, 3000);
