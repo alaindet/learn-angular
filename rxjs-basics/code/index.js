ecc71ff7b3df0417cfe7b0fdded96e4a2a903dd8
Alain D'Ettorre <alain.det@gmail.com>
2020-05-26 18:36:11 +0200

    RxJS: Add missing index.js

---
+++ rxjs-basics/code/index.js
@@ -0,0 +1,10 @@
+import './scripts/getting-started/async-data';
+import './scripts/getting-started/using-callbacks';
+
+import './scripts/creation-operators/from-event';
+import './scripts/creation-operators/from';
+import './scripts/creation-operators/hello-world';
+import './scripts/creation-operators/interval';
+import './scripts/creation-operators/of';
+import './scripts/creation-operators/range';
+import './scripts/creation-operators/timer';
