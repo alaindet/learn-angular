f3409d931108032e434d9fc548f8e81da1b418ed
Alain D'Ettorre <alain.det@gmail.com>
2020-05-26 14:25:48 +0200

    Add first unsubscription

--- rxjs-basics/code/creation-operators/scripts/async-data.js
+++ rxjs-basics/code/creation-operators/scripts/async-data.js
@@ -8,19 +8,9 @@ const observer = {
 
 const observable$ = new Observable(subscriber => {
   let count = 0;
-
-  const id = setInterval(() => {
-    subscriber.next(count);
-    subscriber.complete();
-    count += 1;
-  }, 1000);
-
-  return () => {
-    console.log('called');
-    clearInterval(id);
-  };
+  setInterval(() => subscriber.next(count++), 1000);
 });
 
-console.log('before');
-observable$.subscribe(observer);
-console.log('after');
+const sub = observable$.subscribe(observer);
+
+setTimeout(() => sub.unsubscribe(), 3000);
