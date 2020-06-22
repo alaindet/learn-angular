399512836145dc033cf7e0e7be77feaf66719211
Alain D'Ettorre <alain.det@gmail.com>
2020-06-23 00:30:00 +0200

    Add custom event subscription

--- ngrx/code/course-project/src/store/store.ts
+++ ngrx/code/course-project/src/store/store.ts
@@ -5,6 +5,7 @@ export class Store {
   private state: { [key: string]: any };
 
   constructor(reducers = {}, initialState = {}) {
+    this.subscribers = [];
     this.reducers = reducers;
     this.state = this.reduce(initialState, {});
   }
@@ -15,6 +16,21 @@ export class Store {
 
   dispatch(action: any) {
     this.state = this.reduce(this.state, action);
+    this.notify();
+  }
+
+  subscribe(subscriber) {
+    this.subscribers = [...this.subscribers, subscriber];
+    this.notify();
+    return () => {
+      this.subscribers = this.subscribers.filter(sub => sub !== subscriber);
+    };
+  }
+
+  private notify() {
+    for (const subscriber of this.subscribers) {
+      subscriber(this.value);
+    }
   }
 
   private reduce(state, action) {
