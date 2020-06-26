6145d0463659ed7f17a50ed908d9ddba557fc7ac
Alain D'Ettorre <alain.det@gmail.com>
2020-06-26 08:18:28 +0200

    ngrx: Hooking @ngrx/router-store into the app

--- ngrx/code/angular-ngrx/src/app/app.module.ts
+++ ngrx/code/angular-ngrx/src/app/app.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { StoreModule, MetaReducer } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 
 // not used in production
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { storeFreeze } from 'ngrx-store-freeze';
+
+import { reducers } from './store';
 
 // this would be done dynamically with webpack for builds
 const environment = {
@@ -37,7 +38,7 @@ export const ROUTES: Routes = [
     BrowserModule,
     BrowserAnimationsModule,
     RouterModule.forRoot(ROUTES),
-    StoreModule.forRoot({}, { metaReducers }),
+    StoreModule.forRoot(reducers, { metaReducers }),
     EffectsModule.forRoot([]),
     environment.development ? StoreDevtoolsModule.instrument() : [],
   ],
