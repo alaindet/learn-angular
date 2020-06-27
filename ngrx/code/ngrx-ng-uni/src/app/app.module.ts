443ed70fc32d7192e991e2c18cace2b9175918f4
Alain D'Ettorre <alain.det@gmail.com>
2020-06-27 18:14:10 +0200

    Install @ngrx/store-devtools

--- ngrx/code/ngrx-ng-uni/src/app/app.module.ts
+++ ngrx/code/ngrx-ng-uni/src/app/app.module.ts
@@ -16,6 +16,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthModule } from './auth/auth.module';
 import { StoreModule } from '@ngrx/store';
 import { reducers, metaReducers } from './reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 const routes: Routes = [
   {
@@ -45,6 +47,7 @@ const routes: Routes = [
     MatToolbarModule,
     AuthModule,
     StoreModule.forRoot(reducers, { metaReducers }),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
   ],
   bootstrap: [AppComponent]
 })
