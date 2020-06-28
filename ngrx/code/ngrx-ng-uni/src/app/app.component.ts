42341abe28829fe35d64caf5e55098285d61dedf
Alain D'Ettorre <alain.det@gmail.com>
2020-06-28 11:40:39 +0200

    First read data from store

--- ngrx/code/ngrx-ng-uni/src/app/app.component.ts
+++ ngrx/code/ngrx-ng-uni/src/app/app.component.ts
@@ -1,21 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
+import { Observable, Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Store } from '@ngrx/store';
+
+import { AppState } from './store/reducers';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
 
   loading = true;
+  isLoggedIn$: Observable<boolean>;
+  isLoggedOut$: Observable<boolean>;
+  subs: { [sub: string]: Subscription } = {};
 
   constructor(
     private router: Router,
+    private store: Store<AppState>,
   ) {}
 
   ngOnInit() {
-    this.router.events.subscribe(event  => {
+    this.bindLoadingToRouterEvents();
+    this.bindAuthLinksVisibilityToStore();
+  }
+
+  ngOnDestroy() {
+    for (const sub of Object.values(this.subs)) {
+      sub.unsubscribe();
+    }
+  }
+
+  logout() {
+    console.log('logout');
+  }
+
+  private bindLoadingToRouterEvents() {
+    this.subs['router-events'] = this.router.events.subscribe(event => {
 
       if (event instanceof NavigationStart) {
         this.loading = true;
@@ -34,7 +58,14 @@ export class AppComponent implements OnInit {
     });
   }
 
-  logout() {
-    console.log('logout');
+  private bindAuthLinksVisibilityToStore() {
+
+    this.isLoggedIn$ = this.store.pipe(
+      map((state: AppState) => !!state['auth'].user)
+    );
+
+    this.isLoggedOut$ = this.store.pipe(
+      map((state: AppState) => !state['auth'].user)
+    );
   }
 }
