c6ace25440711c1c66cc36d696187b9d952961e9
Alain D'Ettorre <alain.det@gmail.com>
2020-06-29 08:24:14 +0200

    Add first route guard using store

--- ngrx/code/ngrx-ng-uni/src/app/auth/auth.module.ts
+++ ngrx/code/ngrx-ng-uni/src/app/auth/auth.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule  } from '@angular/material/input';
@@ -7,8 +7,10 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { StoreModule } from '@ngrx/store';
 
-import { authReducer } from './store/reducers';
+import { AuthService } from './auth.service';
+import { AuthGuard } from './auth.guard';
 import { LoginComponent } from './login/login.component';
+import { authReducer } from './store/reducers';
 
 const routes = [
   {
@@ -30,4 +32,14 @@ const routes = [
   declarations: [LoginComponent],
   exports: [LoginComponent]
 })
-export class AuthModule {}
+export class AuthModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: AuthModule,
+      providers: [
+        AuthService,
+        AuthGuard,
+      ],
+    };
+  }
+}
