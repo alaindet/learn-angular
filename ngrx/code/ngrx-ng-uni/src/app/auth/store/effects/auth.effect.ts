9b9a3af6d1b87e544e20723efac05f9aa4ec647b
Alain D'Ettorre <alain.det@gmail.com>
2020-06-29 18:46:47 +0200

    Refactor first use of NGRX effect

--- ngrx/code/ngrx-ng-uni/src/app/auth/store/effects/auth.effect.ts
+++ ngrx/code/ngrx-ng-uni/src/app/auth/store/effects/auth.effect.ts
@@ -1,17 +1,27 @@
 import { Injectable } from '@angular/core';
-import { Actions } from '@ngrx/effects';
+import { Actions, ofType, createEffect } from '@ngrx/effects';
+import { tap } from 'rxjs/operators';
+
+import * as AuthActions from './../actions/';
 
 @Injectable()
 export class AuthEffect {
 
+  login$ = createEffect(
+    () => {
+      return this.actions$.pipe(
+        ofType(AuthActions.login),
+        tap(action => {
+          const key = 'app:user';
+          const data = JSON.stringify(action.user);
+          window.localStorage.setItem(key, data);
+        }),
+      );
+    },
+    { dispatch: false }
+  );
+
   constructor(
     private actions$: Actions,
-  ) {
-    actions$.subscribe(action => {
-      if (action.type === '[Login Page] User Login') {
-        console.log('AuthEffect taking place...');
-        window.localStorage.setItem('user', JSON.stringify(action['user']));
-      }
-    });
-  }
+  ) {}
 }
