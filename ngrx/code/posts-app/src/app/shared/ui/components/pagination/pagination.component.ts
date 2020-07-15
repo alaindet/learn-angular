0b4eccc36980498b1ffb70043e752699f36cc90a
Alain D'Ettorre <alain.dettorre@avanade.com>
2020-07-15 18:38:51 +0200

    Fix <ui-pagination>

--- ngrx/code/posts-app/src/app/shared/ui/components/pagination/pagination.component.ts
+++ ngrx/code/posts-app/src/app/shared/ui/components/pagination/pagination.component.ts
@@ -15,6 +15,7 @@ export class UiPaginationComponent implements OnChanges {
   @Input() current: UiPagination['current'];
   @Input() total: UiPagination['total'];
   @Input() show: UiPagination['show'] = 3;
+  @Input() size: UiPagination['size'] = 'medium';
   @Output() clicked = new EventEmitter<number>();
   pages: Array<number | null> = null;
   previous: number | null = null;
--- ngrx/code/posts-app/src/app/shared/ui/components/pagination/pagination.interface.ts
+++ ngrx/code/posts-app/src/app/shared/ui/components/pagination/pagination.interface.ts
@@ -4,4 +4,5 @@ export interface UiPagination {
   current: UiCommon['asNumber'];
   total: UiCommon['asNumber'];
   show: UiCommon['asNumber'];
+  size: UiCommon['size'];
 }
