3176dfb64243ca464900077b149275d1beb4ce2f
Alain D'Ettorre <alain.dettorre@avanade.com>
2020-07-21 12:52:38 +0200

    Add global styling via sass-pal

--- ngrx-material/fitness-app/src/app/material-components.module.ts
+++ ngrx-material/fitness-app/src/app/material-components.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+
+const components = [
+  MatButtonModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+];
 
 @NgModule({
-  imports: [
-    MatButtonModule,
-    MatIconModule,
-  ],
-  exports: [
-    MatButtonModule,
-    MatIconModule,
-  ],
+  imports: [...components],
+  exports: [...components],
 })
 export class MaterialComponentsModule {}
