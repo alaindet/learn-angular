77f4a0f9d4d0579c39d2c259490cb4186a009f46
Alain D'Ettorre <alain.det@gmail.com>
2020-07-25 16:08:07 +0200

    Add a complete toolbar

--- ngrx-material/fitness-app/src/app/app.component.ts
+++ ngrx-material/fitness-app/src/app/app.component.ts
@@ -1,8 +1,33 @@
 import { Component } from '@angular/core';
 
+interface Link {
+  path: string;
+  label: string;
+  icon: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+
+  links: Link[] = [
+    {
+      path: '/signup',
+      label: 'Signup',
+      icon: 'account_circle',
+    },
+    {
+      path: '/login',
+      label: 'Login',
+      icon: 'login',
+    },
+    {
+      path: '/training',
+      label: 'Training',
+      icon: 'fitness_center',
+    }
+  ];
+}
--- ngrx-material/fitness-app/src/app/material-components.module.ts
+++ ngrx-material/fitness-app/src/app/material-components.module.ts
@@ -6,8 +6,10 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 const components = [
   MatButtonModule,
@@ -16,8 +18,10 @@ const components = [
   MatFormFieldModule,
   MatIconModule,
   MatInputModule,
+  MatListModule,
   MatNativeDateModule,
   MatSidenavModule,
+  MatToolbarModule,
 ];
 
 @NgModule({
