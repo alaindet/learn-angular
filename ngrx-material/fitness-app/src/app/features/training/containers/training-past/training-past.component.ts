25f2fa34cb27525997b9d5bf9c62bf77d87452d6
Alain D'Ettorre <alain.det@gmail.com>
2020-07-31 08:36:04 +0200

    Add past exercises to training service

--- ngrx-material/fitness-app/src/app/features/training/containers/training-past/training-past.component.ts
+++ ngrx-material/fitness-app/src/app/features/training/containers/training-past/training-past.component.ts
@@ -1,8 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { TrainingService } from './../../services/training.service';
+import { Exercise } from './../../models/exercise.model';
 
 @Component({
   selector: 'app-training-past',
   templateUrl: './training-past.component.html',
   styleUrls: ['./training-past.component.scss']
 })
-export class TrainingPastPageComponent {}
+export class TrainingPastPageComponent implements OnInit, OnDestroy {
+
+  exercises: Exercise[] = [];
+  private subs: { [sub: string]: Subscription } = {};
+
+  constructor(
+    private trainingService: TrainingService,
+  ) {}
+
+  ngOnInit() {
+    this.subs.pastExercises = this.trainingService.getPastExercises()
+      .subscribe(
+        (exercises: Exercise[]) => {
+          this.exercises = exercises;
+        }
+      );
+  }
+
+  ngOnDestroy() {
+    for (const sub in this.subs) {
+      this.subs[sub].unsubscribe();
+    }
+  }
+}
--- ngrx-material/fitness-app/src/app/features/training/services/training.service.ts
+++ ngrx-material/fitness-app/src/app/features/training/services/training.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Subject, Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { Exercise } from '../models/exercise.model';
 import { EXERCISES } from '../data/available-exercises.const';
@@ -7,25 +8,73 @@ import { EXERCISES } from '../data/available-exercises.const';
 @Injectable()
 export class TrainingService {
 
-  private currentExercise$ = new Subject<Exercise | null>();
-  private exercises: Exercise[] = EXERCISES;
+  private availableExercises: Exercise[] = EXERCISES;
+  private currentExercise$ = new BehaviorSubject<Exercise | null>(null);
+  private pastExercises$ = new BehaviorSubject<Exercise[]>([]);
 
   getAvailableExercises(): Exercise[] {
-    return this.exercises;
+    return this.availableExercises;
   }
 
   getCurrentExercise(): Observable<Exercise> {
     return this.currentExercise$.asObservable();
   }
 
+  getPastExercises(state?: Exercise['state']): Observable<Exercise[]> {
+
+    if (state) {
+      return this.pastExercises$.pipe(
+        map((exercises: Exercise[]): Exercise[] => {
+          return exercises.filter((exercise: Exercise): boolean => {
+            return exercise.state === state;
+          })
+        })
+      );
+    }
+
+    return this.pastExercises$.asObservable();
+  }
+
   startExercising(id: string) {
-    const exercise = this.exercises.find(ex => ex.id === id);
+    const exercise = this.availableExercises.find(ex => ex.id === id);
     if (exercise) {
       this.currentExercise$.next({ ...exercise });
     }
   }
 
-  stopExercising() {
+  cancelCurrentExercise(progress: number) {
+
+    const current = this.currentExercise$.value;
+
+    const cancelledExercise: Exercise = {
+      ...current,
+      date: new Date(),
+      state: 'cancelled',
+      duration: current.duration * (progress / 100),
+      calories: current.calories * (progress / 100),
+    };
+
+    this.pastExercises$.next([
+      ...this.pastExercises$.value,
+      cancelledExercise,
+    ]);
+
+    this.currentExercise$.next(null);
+  }
+
+  completeCurrentExercise() {
+
+    const completedExercise: Exercise = {
+      ...this.currentExercise$.value,
+      date: new Date(),
+      state: 'completed',
+    };
+
+    this.pastExercises$.next([
+      ...this.pastExercises$.value,
+      completedExercise,
+    ]);
+
     this.currentExercise$.next(null);
   }
 }
