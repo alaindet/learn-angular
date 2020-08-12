3bce1e407b4c16d95d3d4380648298eb6c102ff4
Alain D'Ettorre <alain.det@gmail.com>
2020-08-12 22:31:57 +0200

    Remove previous mannequin-lib library

--- angular/mannequin-ui/projects/mannequin-lib/src/assets/CHANGELOG.md
+++
@@ -1,5 +0,0 @@
-# Mannequin UI Changelog
-
-This file all the relevant changes to this library
-
-## Unreleased
--- angular/mannequin-ui/projects/mannequin-lib/src/public-api.ts
+++
@@ -1,6 +0,0 @@
-/*
- * Public API Surface of mannequin-lib
- */
-export * from './lib/components/alert/alert.component';
-export * from './lib/components/alert/alert.module';
-export * from './lib/components/alert/alert.service';
