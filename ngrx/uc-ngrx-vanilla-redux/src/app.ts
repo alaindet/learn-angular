8a98c37abc4e110d7d3f19c11734b3b474654d14
Alain D'Ettorre <alain.det@gmail.com>
2021-02-23 19:04:58 +0100

    Setup vanilla Redux-like store in TypeScript

--- ngrx/uc-ngrx-vanilla-redux/src/app.ts
+++ ngrx/uc-ngrx-vanilla-redux/src/app.ts
@@ -1,16 +1,9 @@
 import { renderTodos } from './utils';
-import * as fromStore from './store/index';
 
 const input = document.querySelector('input') as HTMLInputElement;
 const button = document.querySelector('button') as HTMLButtonElement;
 const destroy = document.querySelector('.unsubscribe') as HTMLButtonElement;
 const todoList = document.querySelector('.todos') as HTMLLIElement;
-
-const reducers = {
-  todos: fromStore.reducer,
-};
-
-const store = new fromStore.Store(reducers);
 
 button.addEventListener(
   'click',
@@ -19,25 +12,16 @@ button.addEventListener(
 
     const payload = { label: input.value, complete: false };
 
-    store.dispatch(new fromStore.AddTodo(payload));
+    console.log(payload);
 
     input.value = '';
   },
   false
 );
 
-const unsubscribe = store.subscribe(state => {
-  renderTodos(state.todos.data);
-});
-
-destroy.addEventListener('click', unsubscribe, false);
-
 todoList.addEventListener('click', function(event) {
   const target = event.target as HTMLButtonElement;
   if (target.nodeName.toLowerCase() === 'button') {
-    const todo = JSON.parse(target.getAttribute('data-todo') as any);
-    store.dispatch(new fromStore.RemoveTodo(todo));
+    console.log(target);
   }
 });
-
-store.subscribe(state => console.log('STATE::', state));
