75d0d1bc5c8ebde239292453b9384abb3182a336
Alain D'Ettorre <alain.det@gmail.com>
2021-02-23 02:13:43 +0100

    Update store devtools config

--- ngrx/router-demo/src/app/store/devtools/config.ts
+++ ngrx/router-demo/src/app/store/devtools/config.ts
@@ -3,4 +3,5 @@ import { StoreDevtoolsOptions } from '@ngrx/store-devtools';
 export const storeDevtoolsConfig: StoreDevtoolsOptions = {
   name: 'NgRx Router Demo',
   maxAge: 20,
+  logOnly: true,
 };
