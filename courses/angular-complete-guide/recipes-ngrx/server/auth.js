f54f69cbc6fb11960b8d8b58278d54258dc25b8f
Alain D'Ettorre <alain.det@gmail.com>
2021-11-15 18:00:36 +0100

    WIP #10

--- courses/angular-complete-guide/recipes-ngrx/server/auth.js
+++ courses/angular-complete-guide/recipes-ngrx/server/auth.js
@@ -1,3 +1,39 @@
-const router = express.Router();
+const express = require('express');
+const bcrypt = require('bcrypt');
+const database = require('./utils/database');
+const successResponse = require('./utils/success-response');
+const errorResponse = require('./utils/error-response');
 
-module.exports = router;
+const router = express.Router();
+const COLLECTION = 'users';
+
+const FAKE_TOKEN = 'some-super-secret-token';
+
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  const users = await database.fetchCollection(COLLECTION);
+  const user = users.find((user) => user.email === email);
+
+  if (!user) {
+    const message = 'Wrong and/or missing credentials';
+    return res.status(401).json(errorResponse(message));
+  }
+
+  const passwordsMatch = await bcrypt.compare(password, user.password);
+
+  if (!passwordsMatch) {
+    const message = 'Wrong and/or missing credentials';
+    return res.status(401).json(errorResponse(message));
+  }
+
+  const message = 'Login successfull';
+  const data = { token: FAKE_TOKEN };
+  return res.json(successResponse(message, data));
+});
+
+const authMiddleware = (req, res, next) => {
+  console.log("Running auth middlware...");
+  // TODO...
+};
+
+module.exports = { router, authMiddleware };
--- courses/angular-complete-guide/recipes-ngrx/server/server.js
+++ courses/angular-complete-guide/recipes-ngrx/server/server.js
@@ -2,8 +2,8 @@ const path = require('path');
 const express = require('express');
 const cors = require('cors');
 
-// const authRouter = require('./auth');
-// const ingredientsRouter = require('./ingredients');
+const { router: authRouter, authMiddlware } = require('./auth');
+const ingredientsRouter = require('./ingredients');
 const recipesRouter = require('./recipes');
 
 const app = express();
@@ -13,10 +13,11 @@ const PUBLIC_DIR = path.join(__dirname, 'public');
 app.use(cors());
 app.use(express.json());
 app.use(express.static(PUBLIC_DIR));
+app.use(authMiddlware);
 
 app.use('/recipes', recipesRouter);
-// app.use('/ingredients', ingredientsRouter);
-// app.use('/auth', authRouter);
+app.use('/ingredients', ingredientsRouter);
+app.use('/auth', authRouter);
 
 app.listen(PORT, () => {
   console.log(`Recipes API listening at http://localhost:${PORT}`);
