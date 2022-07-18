df7497dd538c5ecb2efa4eb51f0a19bbcb391d52
Alain D'Ettorre <alain.det@gmail.com>
2022-07-18 17:50:58 +0200

    Remove old project

--- experiments/__old__a11ymenu/src/app/a11y-menu/focus.ts
+++
@@ -1,9 +0,0 @@
-export const FOCUSED_HANDLE = 'handle';
-
-export type FocusedHandle = 'handle';
-
-export type FocusedItem = (
-  | FocusedHandle // Focus the menu handle
-  | string // Focus any menu item id
-  | null // Nothing is focused
-);
