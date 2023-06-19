83715ec3fb1f76dab4879000a4da84fabb290670
Alain D'Ettorre <alain.det@gmail.com>
2023-06-19 15:46:34 +0200

    Add draft for reading data from Firebase collection

--- courses/firebase-in-depth/courses-app/src/app/features/settings/settings.component.ts
+++ courses/firebase-in-depth/courses-app/src/app/features/settings/settings.component.ts
@@ -1,31 +1,46 @@
+import { AsyncPipe, JsonPipe, NgIf } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { Firestore, collection, addDoc } from '@angular/fire/firestore';
+import { Firestore, collection, addDoc, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 import { MOCK_COURSES, findLessonsByCourseId } from 'src/mocks';
+
+const imports = [
+  NgIf,
+  AsyncPipe,
+  JsonPipe,
+];
 
 @Component({
   selector: 'app-page-settings',
   templateUrl: './settings.component.html',
   standalone: true,
+  imports,
 })
 export class SettingsPageComponent {
 
   private db = inject(Firestore);
-  private coursesId = 'courses';
-  private lessonsId = 'lessons';
+  private coursesCollectionId = 'courses';
+  private lessonsCollectionId = 'lessons';
+  courses$!: Observable<any[]>;
 
   async onPopulateDatabase() {
-    const coursesCollection = collection(this.db, this.coursesId);
+    const coursesCollection = collection(this.db, this.coursesCollectionId);
 
     for (const course of MOCK_COURSES) {
       const { id: courseId, ...createCourseDto } = course;
       const courseDoc = await addDoc(coursesCollection, createCourseDto);
-      const lessonsCollection = await collection(courseDoc, this.lessonsId);
+      const lessonsCollection = await collection(courseDoc, this.lessonsCollectionId);
 
       for (const lesson of findLessonsByCourseId(courseId)) {
         const { id: lessonId, ...createLessonDto } = lesson;
         await addDoc(lessonsCollection, createLessonDto);
       }
     }
+  }
+
+  onFetchFromDatabase() {
+    const coursesCollection = collection(this.db, this.coursesCollectionId);
+    this.courses$ = collectionData(coursesCollection);
   }
 }
